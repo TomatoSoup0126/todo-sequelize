4b02da3875148b6ce1ebeb90c808ad1aeda0e77b
jonathantang <jonathantang0126@gmail.com>
2019-10-05 23:36:47 +0800

    add routes and views for register and login pages

--- app.js
+++ app.js
@@ -14,8 +14,31 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+
+// 首頁
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.send('hello world')
+})
+// 認證系統的路由
+// 登入頁面
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+// 註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+// 註冊檢查
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 app.listen(port, () => {
