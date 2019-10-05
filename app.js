cebd0d72cb874919ca2848ed61137ca6eebc1335
jonathantang <jonathantang0126@gmail.com>
2019-10-05 23:17:43 +0800

    project start

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
+
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('hello world!')
+})
+
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`)
+})
