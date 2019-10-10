3b9d06d2a2cebc27f09e41c3a2529e915832f1d6
jonathantang <jonathantang0126@gmail.com>
2019-10-10 16:08:48 +0800

    add home and user routers

--- app.js
+++ app.js
@@ -19,36 +19,14 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+app.use('/users', require('./routes/user'))
+
 
 // 首頁
 app.get('/', (req, res) => {
   res.send('hello world')
 })
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+
 
 app.listen(port, () => {
   console.log(`app is running on port ${port}`)
---
+++ routes/user.js
@@ -0,0 +1,51 @@
+// routes/user.js
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+// 載入 user model
+const db = require('../models')
+const User = db.User
+// 登入頁面
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+router.post('/login', (req, res, next) => {
+  res.send('登入檢查')
+})
+// 註冊頁面
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+// 註冊檢查
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body
+  User.findOne({ where: { email: email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      res.render('register', {
+        name,
+        email,
+        password,
+        password2
+      })
+    } else {
+      const newUser = new User({  //  如果 email 不存在就直接新增
+        name,
+        email,
+        password,
+      })
+      newUser
+        .save()
+        .then(user => {
+          res.redirect('/')                   // 新增完成導回首頁
+        })
+        .catch(err => console.log(err))
+    }
+  })
+})
+// 登出
+router.get('/logout', (req, res) => {
+  res.send('logout')
+})
+module.exports = router
