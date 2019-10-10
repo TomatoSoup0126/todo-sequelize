05cfa46e74d4dd3ef251b2667493f929ca9d784c
jonathantang <jonathantang0126@gmail.com>
2019-10-10 15:47:51 +0800

    add mysql database

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const passport = require('passport')
 const app = express()
 const port = 3000
 
+// 載入 model
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
@@ -34,7 +39,11 @@ app.get('/users/register', (req, res) => {
 })
 // 註冊檢查
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }).then(user => res.redirect('/'))
 })
 // 登出
 app.get('/users/logout', (req, res) => {
