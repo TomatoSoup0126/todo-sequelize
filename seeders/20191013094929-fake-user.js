0c01c3cd4fc809eeb8ac0e0f20e911cf2430abe5
jonathantang <jonathantang0126@gmail.com>
2019-10-18 01:13:00 +0800

    add seeder

--- seeders/20191013094929-fake-user.js
+++ seeders/20191013094929-fake-user.js
@@ -5,8 +5,21 @@ const todos = require('./fakeTodo.json')
 const bcrypt = require('bcryptjs')
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
+    users.forEach(user => {
+      //雜湊password
+      let hash = bcrypt.hashSync(user.password, 10)
+      user.password = hash
+      user.createdAt = new Date()
+      user.updatedAt = new Date()
+    })
 
+    todos.forEach(todo => {
+      todo.createdAt = new Date()
+      todo.updatedAt = new Date()
+    })
+    await queryInterface.bulkInsert('Users', users)
+    await queryInterface.bulkInsert('Todos', todos)
   },
 
   down: (queryInterface, Sequelize) => {
